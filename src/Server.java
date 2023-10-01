69a5e8e6957f8dd5d6baed3507a951d09af77ea1
SoslanZ <zagegovski@yandex.ru>
2023-10-01 20:26:56 +0300

    server

---
+++ src/Server.java
@@ -0,0 +1,32 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        ConsoleHelper.writeMessage("Введите порт сервера:");
+        int port = ConsoleHelper.readInt();
+        try(ServerSocket serverSocket = new ServerSocket(port)) {
+            ConsoleHelper.writeMessage("Сервер запущен.");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                handler.start();
+            }
+        } catch (Exception e) {
+            ConsoleHelper.writeMessage("Произошла ошибка при запуске или работе сервера.");
+        }
+    }
+
+    private static class Handler extends Thread{
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+
+        }
+    }
+}
